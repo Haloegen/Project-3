d3fabda9e19eab6ada65d090fc69e75a3949b5cb
Haloegen <>
2023-09-28 22:34:00 +0000

    Create list of possible functions for project

--- run.py
+++ run.py
@@ -5,8 +5,20 @@
 HIDDEN_BOARD = [[''] * 8 for x in range (8)]
 GUESS_BOARD = [[''] * 8 for x in range(8)]
 
-letters_to_numbers = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
 
 def board():
-    pass
+    
+
+
+def create_ships():
+    
+
+
+def get_ship_location():
+    
+
+
+def count_hit_ships():
+    
