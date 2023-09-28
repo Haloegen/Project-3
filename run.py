567fa5df33641a642fe1b3dedb09ef0891bb5137
Haloegen <>
2023-09-28 22:28:39 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+HIDDEN_BOARD = [[''] * 8 for x in range (8)]
+GUESS_BOARD = [[''] * 8 for x in range(8)]
+
+letters_to_numbers = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+
+
+def board():
+    pass
