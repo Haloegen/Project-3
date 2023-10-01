432d886fb03117ed331b1cad4e0d800b98f268ce
Haloegen <>
2023-10-01 14:22:00 +0000

    added code to create ships

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
 
 HIDDEN_BOARD = [[''] * 8 for x in range (8)]
 GUESS_BOARD = [[''] * 8 for x in range(8)]
@@ -9,15 +10,20 @@ letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H
 
 
 def print_board(board):
-    print('   A B C D E F G H')
-    print('   ---------------')
+    print("  A B C D E F G H")
+    print("  +-+-+-+-+-+-+-+")
     row_number = 1
     for row in board:
-        print("")
+        print("%d|%s" % (row_number, "|".join(row)))
+        row_number += 1
 
 
-def create_ships():
-    pass
+def create_ships(board):
+    for ship in range(5):
+        ship_row, ship_column = randint(0,7)
+        while board[ship_row][ship_column] == 'X':
+            ship_row, ship_column = randint(0,7), randint(0,7)
+        board[ship_row][ship_column] = 'X'
     
 
 
